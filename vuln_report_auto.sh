0cfef8de8199068c09a932f1a870d67631bd4a68
Boris <39199196+borross@users.noreply.github.com>
2024-07-19 17:19:00 +0300

    Update vuln_report_auto.sh

--- vuln_report_auto.sh
+++ vuln_report_auto.sh
@@ -12,7 +12,7 @@ for i in $(find $REPORT_PATH -name "*.xml" -type f -exec cksum {} \;); do
 	if ! grep -e $HASH $PROCESS_LIST; then
 		# echo "NOT in list"
 		echo $i >> $PROCESS_LIST
-		/root/maxpatrol-tool --kuma-rest localhost:7223 --token $TOKEN_FILE --tenant Main $FILE
+		/root/kuma-ansible-installer/roles/kuma/files/maxpatrol-tool --kuma-rest localhost:7223 --token $TOKEN_FILE --tenant Main $FILE
 		write_to_log=$(echo "file: $FILE with hash: $HASH processed" | systemd-cat -t maxpatrol-tool -p info)
 	fi
 done
